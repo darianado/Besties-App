a4adf3d00b1f21517c9a1d2f2adb3f8a85fcfe68
Theodora Safta <theodorasafta@yahoo.ro>
2022-02-10 19:06:30 +0000

    Beginning of signup 2

--- lib/sign_up2.dart
+++ lib/sign_up2.dart
@@ -1,22 +1,215 @@
 import 'package:flutter/material.dart';
+import 'package:project_seg/reusableCard.dart';
+import 'constants.dart';
+import 'iconContent.dart';
 
-class Sign_Up2 extends StatelessWidget {
+enum Gender {
+  male,
+  female
+}
+
+class Sign_Up2 extends StatefulWidget {
+  @override
+  _Sign_Up2State createState() => _Sign_Up2State();
+
+}
+
+class _Sign_Up2State extends State<Sign_Up2> {
+
+  final GlobalKey _key = GlobalKey<FormState>();
+  final TextEditingController _firstName = TextEditingController();
+  final TextEditingController _lastName = TextEditingController();
+  final TextEditingController _dob = TextEditingController();
+  final TextEditingController _gender = TextEditingController();
+
+  DateTime selectedDate = DateTime.now();
+  bool dateChanged = false;
+  Gender selectedGender = Gender.male;
+
+  _selectDate(BuildContext context) async {
+     DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate, // Refer step 1
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2025),
+    );
+    if (picked != null && picked != selectedDate) {
+      dateChanged = true;
+      setState(() {
+        selectedDate = picked;
+
+      }
+    );
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _firstName.dispose();
+    _lastName.dispose();
+    _dob.dispose();
+    _gender.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text('Screen 2'),
+        backgroundColor: Colors.purple,
+        title: const Text('Sign Up'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Go Back To Screen 1'),
-          onPressed: () {
-            Navigator.pop(context);
-          },
+
+      body:
+      Form (
+        key: _key,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget> [
+              const Padding(
+                padding: EdgeInsets.only(bottom: 15, left: 10, right: 10),
+                child: Text('About you',
+                    style: TextStyle(
+                        fontSize: 30.0
+                    ) ),
+              ),
+              const SizedBox(height: 50),
+              Row(
+                children: <Widget> [
+                  Icon(
+                    Icons.account_circle_outlined,
+                    size: 24.0,
+                  ),
+                  SizedBox (width: 10),
+                  Expanded(
+                    child: TextFormField(
+                    controller: _firstName,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        labelText: 'First Name'
+                    ),
+                    validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                      textInputAction: TextInputAction.next,
+                    maxLines: null,
+                    keyboardType: TextInputType.multiline,
+                  ),
+                  ),
+                  SizedBox (width: 10),
+                  Expanded(
+                    child: TextFormField(
+                      controller: _firstName,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          labelText: 'Last Name'
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                      textInputAction: TextInputAction.next,
+                      maxLines: null,
+                      keyboardType: TextInputType.multiline,
+                    ),
+                  ),
+                  SizedBox (width: 10),
+                ],
+              ),
+              Expanded(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Expanded(
+                        child: Text(
+                          'Date of birth: ',
+                          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      // SizedBox(
+                      //   width: 20.0,
+                      // ),
+                      Expanded(
+                        child: FlatButton(
+                          onPressed: () => _selectDate(context), // Refer step 3
+                          child: Text(
+                            dateChanged == false ? 'Select date' : "${selectedDate.toLocal()}".split(' ')[0],
+                            style:
+                            TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                          ),
+                          color: Colors.blue.shade500,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                    ],
+                  ),
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: ReusableCard(
+                          color: selectedGender == Gender.male
+                              ? kActiveCardColor
+                              : kInactiveCardColor,
+                          cardChild: IconContent(
+                            icon: new Icon(Icons.lock),
+                            label: 'MALE',
+                          ),
+                            onPress: () => setState(() {
+                              selectedGender = Gender.male;
+                            })
+                        ),
+                    ),
+                    Expanded(
+                      child: ReusableCard(
+                        color: selectedGender == Gender.female
+                            ? kActiveCardColor
+                            : kInactiveCardColor,
+                        cardChild: IconContent(
+                          icon: new Icon(Icons.lock),
+                          label: 'FEMALE',
+                        ),
+                          onPress: () => setState(() {
+                            selectedGender = Gender.female;
+                          })
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox (height: 30),
+              ElevatedButton(
+                onPressed: (){
+                  if(((_key.currentState as FormState).validate()) == true) {
+                    Navigator.pushNamed(context, '/signup3');
+                  }
+
+                },
+                child: const Text(" NEXT"),
+              )
+
+            ]
         ),
+
       ),
     );
   }
 }
+
+
 

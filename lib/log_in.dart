37e77f932f1fc1aed51e5bc479b92e4aca7fc9b1
YueYu-amy <k20005314@kcl.ac.uk>
2022-02-17 16:40:24 +0000

    finish the looking for the login page

---
+++ lib/log_in.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:email_validator/email_validator.dart';
+
+class Log_In extends StatefulWidget {
+  const Log_In({Key? key}) : super(key: key);
+
+  @override
+  _Log_InState createState() => _Log_InState();
+}
+
+class _Log_InState extends State<Log_In> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: const Text('Log In'),
+      ),
+
+      body:
+      Form (
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget> [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 15, left: 10, right: 10),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      icon: const Icon(Icons.email),
+                      labelText: 'Email address:'),
+                  textInputAction: TextInputAction.next,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 15, left: 10, right: 10),
+                child: TextFormField(
+                  obscureText: true,
+                  decoration:  InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    icon: new Icon(Icons.lock),
+                    labelText: 'Password',
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: (){
+                      Navigator.pushNamed(context, '/');
+                  },
+                  child: Text("Log In")
+              ),
+              TextButton(
+                  child: Text('New here? Sign up now'),
+                onPressed: (){
+                  Navigator.pushNamed(context, '/');
+                },
+              ),
+            ]
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'sign_up1.dart';
 import 'sign_up2.dart';
 import 'sign_up3.dart';
 import 'sign_up4.dart';
+import 'log_in.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,12 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/',
+      initialRoute: '/login',
       routes: {
         '/': (context) => Sign_Up1(),
         '/first': (context) => Sign_Up2(),
         '/signup3' : (context) => Sign_Up3(),
         '/signup4' : (context) => Sign_Up4(),
+        '/login' : (context) => Log_In(),
       },
     );
   }

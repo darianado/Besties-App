eb90452463a2c1d93c650bd06d39cd29a70c2c8f
dariana <darianadorin@yahoo.com>
2022-03-01 15:16:43 +0000

    landing page

--- lib/landingPage.dart
+++ lib/landingPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'widgets.dart';
 
 class LandingPage extends StatefulWidget {
@@ -10,57 +11,83 @@ class LandingPage extends StatefulWidget {
 class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
+
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Container(
       decoration: const BoxDecoration(
           gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            colors: [
-              // Color(0xFF827081),
-              Color(0xFF31081F),
-              Color(0xFFDCE0D9)
-            ],
+        begin: Alignment.topRight,
+        end: Alignment.bottomLeft,
+        stops: [0.1, 0.2, 1],
+        colors: [
+          // Color(0xFF827081),
+          Color(0xFF00CFFF),
+          Color(0xFF01B3E0),
+          Color(0xFF041731),
+        ],
       )),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          backgroundColor: Colors.black45,
-          title: const Text('Sign Up'),
-        ),
+        appBar: PreferredSize(
+          preferredSize:Size.fromHeight(0.1 * screenHeight) , // here the desired height
+          child:AppBar(
+          backgroundColor:  Colors.black45,
+          systemOverlayStyle: const SystemUiOverlayStyle(
+                statusBarColor: Colors.transparent, 
+          ),
+          title: const Text(
+            'BESTIES',
+            style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+            ),
+          centerTitle: true,
+          automaticallyImplyLeading: false
+        ),),
         body: Center(
           child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 Text('LOGO'),
-                const SizedBox(height: 150),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/login');
-                  },
-                  child: const Text(" LOG IN"),
-                  style: ElevatedButton.styleFrom(
-                      primary: Color(0xFFCC8B86),
-                      fixedSize: const Size(300, 100),
-                      shadowColor: Color(0xFF7A306C),
-                      elevation: 12,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50))),
+                const SizedBox(height: 450),
+                SizedBox(
+                  width: 0.85 * screenWidth,
+                  height: 0.08 * screenHeight,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/login');
+                    },
+                    child: const Text(" LOG IN"),
+                    style: ElevatedButton.styleFrom(
+                        primary: Color(0xFF0083A1),
+                        fixedSize: const Size(300, 100),
+                        shadowColor: Color(0xFF041731),
+                        elevation: 12,
+                        textStyle: const TextStyle(fontSize: 20),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50))),
+                  ),
                 ),
                 const SizedBox(height: 30),
-                ElevatedButton(
+                 SizedBox(
+                  width: 0.85 * screenWidth,
+                  height: 0.08 * screenHeight,
+                  child: ElevatedButton(
                   onPressed: () {
                     Navigator.pushNamed(context, '/');
                   },
                   child: const Text("SIGN UP"),
                   style: ElevatedButton.styleFrom(
-                      primary: Color(0xFF7A306C),
+                      primary: Color(0xFFFEFCFB),
+                      onPrimary: Color(0xFF041731),
                       fixedSize: const Size(300, 100),
-                      shadowColor: Color(0xFFCC8B86),
+                      shadowColor: Color(0xFF041731),
                       elevation: 12,
+                      textStyle: const TextStyle(fontSize: 20),
                       shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(50))),
-                ),
+                 )),
               ]),
         ),
       ),

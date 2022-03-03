968379f41908c82b06913571870e8e01783ed398
Sze Wye Tham <sze_wye.tham@kcl.ac.uk>
2022-03-03 17:42:45 +0000

    Design for log in page

--- lib/log_in.dart
+++ lib/log_in.dart
@@ -67,83 +67,107 @@ class _LogInState extends State<LogIn> {
             ),
             body: Center(
                 child: SingleChildScrollView(
-              child: Form(
-                key: _formKey,
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            bottom: 15, left: 10, right: 10),
-                        child: TextFormField(
-                          controller: _email,
-                          decoration: const InputDecoration(
-                              border: UnderlineInputBorder(),
-                              icon: Icon(
-                                Icons.email,
-                                color: Colors.white,
-                              ),
-                              labelText: 'Email address'),
-                          validator: (value) =>
-                              !isEmail(_email.text) ? "Invalid Email" : null,
-                          textInputAction: TextInputAction.next,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            bottom: 15, left: 10, right: 10),
-                        child: TextFormField(
-                          controller: _password,
-                          obscureText: true,
-                          decoration: const InputDecoration(
-                            border: UnderlineInputBorder(),
-                            icon: Icon(
-                              Icons.lock,
-                              color: Colors.white,
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          const Padding(
+                            padding: const EdgeInsets.fromLTRB(22.0, 35.0, 22.0, 35.0),
+                            child: Text(
+                              'Log in',
+                              style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
                             ),
-                            labelText: 'Password',
                           ),
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter some text';
-                            }
-                            return null;
-                          },
-                          textInputAction: TextInputAction.next,
-                        ),
-                      ),
-                      SizedBox(
-                          width: 0.85 * screenWidth,
-                          height: 0.08 * screenHeight,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              if (((_formKey.currentState as FormState)
-                                      .validate()) ==
-                                  true) {
-                                _loginAccount(_email.text, _password.text);
-                              }
-                            },
-                            child: const Text("Log In"),
-                            style: ElevatedButton.styleFrom(
-                                primary: Color(0xFFFEFCFB),
-                                onPrimary: Color(0xFF041731),
-                                fixedSize: const Size(300, 100),
-                                shadowColor: Color(0xFF041731),
-                                elevation: 12,
-                                textStyle: const TextStyle(
-                                    fontSize: 25, fontWeight: FontWeight.bold),
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(50))),
-                          )),
-                      TextButton(
-                        child: Text('New here? Sign up now'),
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/');
-                        },
-                      ),
-                    ]),
-              ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(30.0, 35.0, 30.0, 5.0),
+                            child: TextFormField(
+                              controller: _email,
+                              decoration: const InputDecoration(
+                                  border: UnderlineInputBorder(),
+                                  icon: Icon(
+                                    Icons.email,
+                                    color: Colors.white,
+                                  ),
+                                  labelText: 'Email address'),
+                              validator: (value) =>
+                                  !isEmail(_email.text) ? "Invalid Email" : null,
+                              textInputAction: TextInputAction.next,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(30.0, 5.0, 30.0, 40.0),
+                            child: TextFormField(
+                              controller: _password,
+                              obscureText: true,
+                              decoration: const InputDecoration(
+                                border: UnderlineInputBorder(),
+                                icon: Icon(
+                                  Icons.lock,
+                                  color: Colors.white,
+                                ),
+                                labelText: 'Password',
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter some text';
+                                }
+                                return null;
+                              },
+                              textInputAction: TextInputAction.next,
+                            ),
+                          ),
+                          SizedBox(
+                              width: 0.85 * screenWidth,
+                              height: 0.08 * screenHeight,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  if (((_formKey.currentState as FormState)
+                                          .validate()) ==
+                                      true) {
+                                    _loginAccount(_email.text, _password.text);
+                                  }
+                                },
+                                child: const Text("Log In"),
+                                style: ElevatedButton.styleFrom(
+                                    primary: Color(0xFFFEFCFB),
+                                    onPrimary: Color(0xFF041731),
+                                    fixedSize: const Size(300, 100),
+                                    shadowColor: Color(0xFF041731),
+                                    elevation: 12,
+                                    textStyle: const TextStyle(
+                                        fontSize: 25, fontWeight: FontWeight.bold),
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(50))),
+                              )),
+                          Container(
+                            padding: const EdgeInsets.fromLTRB(35.0, 35.0, 35.0, 35.0),
+                            child: Row(
+                              children: <Widget>[
+                                const Expanded(
+                                  flex: 2,
+                                  child: Text('Don’t have an account with us?',),
+                                ),
+                                Expanded(
+                                  flex: 1,
+                                  child: OutlinedButton(
+                                    onPressed: () {
+                                      Navigator.pushNamed(context, '/');
+                                    },
+                                    style: OutlinedButton.styleFrom(
+                                      primary: const Color(0xFFFEFCFB),
+                                      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
+                                      side: BorderSide(color: Color(0xFFFEFCFB), width: 1),
+                                    ),
+                                    child: const Text("Sign up", style: TextStyle(color: Color(0xFFFEFCFB))),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )
+                        ]),
+                  ),
             ))));
   }
 }

b22b1d0d432684d4e24a0cc211e07c831d6e84ba
Adamski, Mateusz (k20063061) <mateusz.adamski@kcl.ac.uk>
2022-03-03 20:10:15 +0000

    Implement fundaments for interest selection in sign_up4.

--- lib/Sign_Up/sign_up4.dart
+++ lib/Sign_Up/sign_up4.dart
@@ -12,15 +12,71 @@ class SignUp4 extends StatelessWidget {
           color: Colors.white,
         ),
       ),
-
       body: Center(
-        child: ElevatedButton(
-          child: Text('Go Back To Screen 3'),
-          onPressed: () {
-            Navigator.pop(context);
-          },
+        child: Column(
+          children: [
+            ElevatedButton(
+              child: Text('Go Back To Screen 3'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            const SizedBox(height: 30),
+            Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: InterestStatus(),
+                  ),
+                ]),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class InterestStatus extends StatefulWidget {
+  @override
+  State<InterestStatus> createState() => _InterestStatusState();
+}
+
+class _InterestStatusState extends State<InterestStatus> {
+  String dropdownValue = 'Select your first interest';
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      title: Text(dropdownValue),
+      children: <Widget>[
+        DropdownButton(
+            value: 'Cat1',
+            items: <String>['Cat1', 'SubCat1', 'SubCat2', 'SubCat3']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+            onChanged: (String? newValue) {
+              setState(() {
+                dropdownValue = newValue!;
+              });
+            }),
+        DropdownButton(
+            value: 'Cat2',
+            items: <String>['Cat2', 'SubCat1', 'SubCat2', 'SubCat3']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+            onChanged: (String? newValue) {
+              setState(() {
+                dropdownValue = newValue!;
+              });
+            })
+      ],
     );
   }
 }

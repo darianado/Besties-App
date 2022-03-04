3a1c9afc30f6bfc92924fd1afbb192190b7fb776
theodorasafta <theodorasafta@yahoo.ro>
2022-03-04 18:53:27 +0000

    Added text info and fixed scroll

--- lib/Sign_Up/sign_up4.dart
+++ lib/Sign_Up/sign_up4.dart
@@ -17,21 +17,35 @@ class SignUp4 extends StatelessWidget {
           color: Colors.white,
         ),
       ),
-      body: Center(
-        child: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(height: 30),
-            Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Expanded(
-                    child: InterestStatus(),
-                  ),
-                ]),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 40),
+              Text(
+                  "It is time to choose categories you are interested in.",
+                   textAlign: TextAlign.center,
+              ),
+              Text(
+                "Please select at least one interest.",
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                "The maximum number of categories you can select is 10",
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 50),
+              Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: InterestStatus(),
+                    ),
+                  ]),
+            ],
+          ),
         ),
-      ),
       ),
     );
   }

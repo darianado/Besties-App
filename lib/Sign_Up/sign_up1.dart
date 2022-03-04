c0ab2fc736fa4390ecb9dae290ee9d9a0060dbd8
Sze Wye Tham <sze_wye.tham@kcl.ac.uk>
2022-03-04 15:24:26 +0000

    Design sign up 1

--- lib/Sign_Up/sign_up1.dart
+++ lib/Sign_Up/sign_up1.dart
@@ -95,14 +95,14 @@ class _SignUp1State extends State<SignUp1> {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget> [
                     const Padding(
-                      padding: EdgeInsets.fromLTRB(22.0, 35.0, 22.0, 35.0),
+                      padding: EdgeInsets.fromLTRB(22.0, 28.0, 22.0, 30.0),
                       child: Text(
                         'Sign up',
                         style: TextStyle(fontSize: 35, color: Color(0xFF004376), fontWeight: FontWeight.bold),
                       ),
                     ),
                     Padding(
-                      padding: const EdgeInsets.fromLTRB(30.0, 35.0, 30.0, 5.0),
+                      padding: const EdgeInsets.only(left: 30.0, right: 30.0, bottom: 25.0),
                       child: TextFormField(
                         controller: _email,
                         decoration: const InputDecoration(
@@ -111,13 +111,13 @@ class _SignUp1State extends State<SignUp1> {
                               Icons.email,
                               color: Color(0xFF004376)
                             ),
-                            labelText: 'Email address:'),
+                            labelText: 'Email address'),
                           validator: (value) => !isEmail(_email.text) ? "Invalid Email" : null,
                         textInputAction: TextInputAction.next,
                       ),
                     ),
                     Padding(
-                      padding: const EdgeInsets.fromLTRB(30.0, 35.0, 30.0, 5.0),
+                      padding: const EdgeInsets.only(left: 30.0, right: 30.0, bottom: 25.0),
                       child: TextFormField(
                         controller: _password,
                         obscureText: true,
@@ -139,7 +139,7 @@ class _SignUp1State extends State<SignUp1> {
                       ),
                     ),
                     Padding(
-                      padding: const EdgeInsets.fromLTRB(30.0, 35.0, 30.0, 5.0),
+                      padding: const EdgeInsets.only(left: 30.0, right: 30.0, bottom: 40.0),
                       child: TextFormField(
                         controller: _confirmPassword,
                         obscureText: true,
@@ -163,16 +163,72 @@ class _SignUp1State extends State<SignUp1> {
                       ),
                     ),
 
-                  const SizedBox(),
-                  ElevatedButton(
-                      onPressed: (){
+                    SizedBox(
+                        width: 0.80 * screenWidth,
+                        height: 0.07 * screenHeight,
+                        child: ElevatedButton(
+                          onPressed: (){
 
-                        if(((_formKey.currentState as FormState).validate()) == true) {
-                          _createAccount(_email.text, _password.text);
-                        }
-                      },
-                      child: Text(" NEXT"),
-                  ),
+                            if(((_formKey.currentState as FormState).validate()) == true) {
+                              _createAccount(_email.text, _password.text);
+                            }
+                          },
+                          child: const Text("Next"),
+                          style: ElevatedButton.styleFrom(
+                              primary: const Color(0xFF041731),
+                              onPrimary: const Color(0xFFFEFCFB),
+                              fixedSize: const Size(300, 100),
+                              shadowColor: const Color(0xFF041731),
+                              elevation: 12,
+                              textStyle: const TextStyle(
+                                  fontSize: 25, fontWeight: FontWeight.bold),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50))),
+                        )),
+
+
+                  // const SizedBox(),
+                  // ElevatedButton(
+                  //     onPressed: (){
+                  //
+                  //       if(((_formKey.currentState as FormState).validate()) == true) {
+                  //         _createAccount(_email.text, _password.text);
+                  //       }
+                  //     },
+                  //     child: Text(" NEXT"),
+                  // ),
+
+                    Container(
+                      padding: const EdgeInsets.all(35.0),
+                      child: Row(
+                        children: <Widget>[
+                          const Expanded(
+                            flex: 2,
+                            child: Text(
+                              'Have an account? Log in now?',
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: OutlinedButton(
+                              onPressed: () {
+                                Navigator.pushNamed(context, '/login');
+                              },
+                              style: OutlinedButton.styleFrom(
+                                primary: const Color(0xFF041731),
+                                shape: const RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.all(
+                                        Radius.circular(30))),
+                                side: const BorderSide(
+                                    color: Color(0xFF041731), width: 1.5),
+                              ),
+                              child: const Text("Log in",
+                                  style: TextStyle(color: Color(0xFF041731))),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
 
               ]
             ),

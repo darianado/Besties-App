c80ebd595cc4f5ec82f5fa02b39b334323a27038
Sze Wye Tham <sze_wye.tham@kcl.ac.uk>
2022-03-07 23:50:56 +0000

    Design for user profile page

--- lib/Personal_information/profile_page.dart
+++ lib/Personal_information/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_seg/constants.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -10,97 +11,139 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.purple,
-        title: const Text('Your Profile'),
-      ),
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
 
+    return Scaffold(
+      backgroundColor: kWhiteColour,
       body: SingleChildScrollView(
-      child: Form (
-        child: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 100,
-                  backgroundImage: AssetImage("assets/images/empty_profile_picture.jpg"),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  height: 0.3 * screenHeight,
+                  width: screenWidth,
+                  decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: [0.65, 1],
+                    colors: [kTertiaryColour, kLightBlue],
+                  )),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 35.0, right: 10.0),
+                    child: Align(
+                      alignment: Alignment.topRight,
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.edit,
+                          color: kWhiteColour,
+                        ),
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/edit_profile');
+                        },
+                      ),
+                    ),
+                  ),
                 ),
-                SizedBox(
-                  height: 40,
+                Padding(
+                  padding: const EdgeInsets.only(top: 120.0, bottom: 30.0),
+                  child: Center(
+                    child: CircleAvatar(
+                      radius: 100,
+                      backgroundImage:
+                          AssetImage("assets/images/empty_profile_picture.jpg"),
+                    ),
+                  ),
                 ),
-                Text(
-                  "Name",
-                  style: TextStyle(
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                'Name',
+                style: TextStyle(
+                  color: kTertiaryColour,
                   fontSize: 40.0,
                   fontWeight: FontWeight.bold,
-                  fontFamily: 'Pacifico',
                 ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(right: 8.0),
+                    child: Icon(
+                      Icons.school_outlined,
+                      color: kTertiaryColour,
+                    ),
+                  ),
+                  Text(
+                    'University',
+                    style: const TextStyle(
+                      fontSize: 30,
+                      color: kTertiaryColour,
+                    ),
+                  ),
+                ]
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text(
+                "21",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: kTertiaryColour,
                 ),
-                SizedBox(
-                  height: 20,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text(
+                "Bio",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: kTertiaryColour,
                 ),
-                Text(
-                  "Age",
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Pacifico',
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text(
+                "Preferences",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: kTertiaryColour,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 30.0),
+              child: SizedBox(
+                width: 0.80 * screenWidth,
+                height: 0.07 * screenHeight,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/edit_preferences');
+                  },
+                  child: const Text("Edit preferences", style: TextStyle(
+                    fontSize: 20,
+                  ),),
+                  style: ElevatedButton.styleFrom(
+                    primary: kTertiaryColour,
+                    onPrimary: kWhiteColour,
+                    shadowColor: kTertiaryColour,
+                    elevation: 12,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
                   ),
                 ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  "Bio",
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Pacifico',
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  "Preferences",
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Pacifico',
-                  ),
-                ),
-                SizedBox(
-                  height: 40,
-                ),
-                Row(
-                  children: [
-                    SizedBox(
-                      width: 10,
-                    ),
-                    ElevatedButton(
-                      onPressed: (){
-                        Navigator.pushNamed(context, '/edit_profile');
-                      },
-                      child: const Text("EDIT YOUR PROFILE"),
-                    ),
-                    SizedBox(
-                      width: 30,
-                    ),
-                    ElevatedButton(
-                      onPressed: (){
-                        Navigator.pushNamed(context, '/edit_preferences');
-                      },
-                      child: const Text("EDIT PREFRENCES"),
-                    ),
-                  ],
-                ),
-
-
-              ]
-          ),
+              ),
+            ),
+          ],
         ),
-      ),
       ),
     );
   }

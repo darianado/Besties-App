79e656d37a8b15ab27b1364d1adef108356a37e1
Luca Brown <luca.brown@kcl.ac.uk>
2022-03-17 15:02:19 +0000

    Add modal bottom sheet scrolling physics

--- lib/models/profile_container.dart
+++ lib/models/profile_container.dart
@@ -2,9 +2,13 @@ import 'dart:collection';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:project_seg/models/User/UserData.dart';
+import 'package:scroll_to_index/scroll_to_index.dart';
 import '../constants.dart';
 import 'package:blur/blur.dart';
+import 'package:scroll_to_index/scroll_to_index.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 import '../screens/components/buttons/bio_field.dart';
 import '../screens/components/buttons/edit_dob_button.dart';
@@ -176,95 +180,129 @@ class PartialProfileDetails extends StatelessWidget {
 }
 
 // Widget that displays all of the profile's details as a sliding bottom sheet.
-class CompleteProfileDetails extends StatelessWidget {
+class CompleteProfileDetails extends StatefulWidget {
   final UserData profile;
 
   const CompleteProfileDetails({Key? key, required this.profile})
       : super(key: key);
 
   @override
+  State<CompleteProfileDetails> createState() => _CompleteProfileDetailsState();
+}
+
+class _CompleteProfileDetailsState extends State<CompleteProfileDetails> {
+  late AutoScrollController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AutoScrollController(
+        viewportBoundaryGetter: () =>
+            Rect.fromLTRB(0, 0, 0, MediaQuery.of(context).padding.bottom),
+        axis: Axis.vertical);
+  }
+
+  Future<dynamic> _scrollBackToTop(details) async {
+    await controller.scrollToIndex(0,
+        duration: const Duration(milliseconds: 500),
+        preferPosition: AutoScrollPosition.begin);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.50,
-      child: ListView(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Column(
-              children: [
-                Text(
-                  profile.fullName ?? " ",
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    color: kTertiaryColour,
-                    fontSize: 40.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                UniversityButton(
-                  label: profile.university ?? " ",
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Wrap(
-                  spacing: 6.0,
-                  runSpacing: 6.0,
-                  alignment: WrapAlignment.center,
-                  runAlignment: WrapAlignment.center,
-                  children: [
-                    DateOfBirthButton(label: (profile.age ?? " ").toString()),
-                    GenderButtton(label: profile.gender ?? " "),
-                    RelationshipStatusButton(
-                        label: profile.relationshipStatus ?? " "),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                BioField(
-                  label: profile.bio ?? " ",
-                  editable: false,
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+      height: MediaQuery.of(context).size.height * 0.43,
+      child: Listener(
+        onPointerUp: (details) => _scrollBackToTop(details),
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          controller: controller,
+          physics: const ClampingScrollPhysics(),
+          children: [
+            AutoScrollTag(
+              key: const ValueKey(0),
+              index: 0,
+              controller: controller,
+              child: Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Column(
                   children: [
                     Text(
-                      "YOU HAVE",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 12,
-                        color: kPrimaryColour.withOpacity(0.3),
-                      ),
-                    ),
-                    const Text(
-                      " NO ",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 12,
+                      widget.profile.fullName ?? " ",
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
                         color: kTertiaryColour,
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    Text(
-                      "INTERESTS IN COMMON!",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 12,
-                        color: kPrimaryColour.withOpacity(0.3),
-                      ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    UniversityButton(
+                      label: widget.profile.university ?? " ",
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Wrap(
+                      spacing: 6.0,
+                      runSpacing: 6.0,
+                      alignment: WrapAlignment.center,
+                      runAlignment: WrapAlignment.center,
+                      children: [
+                        DateOfBirthButton(
+                            label: (widget.profile.age ?? " ").toString()),
+                        GenderButtton(label: widget.profile.gender ?? " "),
+                        RelationshipStatusButton(
+                            label: widget.profile.relationshipStatus ?? " "),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    BioField(
+                      label: widget.profile.bio ?? " ",
+                      editable: false,
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "YOU HAVE",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12,
+                            color: kPrimaryColour.withOpacity(0.3),
+                          ),
+                        ),
+                        const Text(
+                          " 3 ",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12,
+                            color: kTertiaryColour,
+                          ),
+                        ),
+                        Text(
+                          "INTERESTS IN COMMON!",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12,
+                            color: kPrimaryColour.withOpacity(0.3),
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }

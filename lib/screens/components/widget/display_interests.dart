7ace5558baee2e4f216067db319be8022ce466b5
Sze Wye Tham <sze_wye.tham@kcl.ac.uk>
2022-03-24 15:45:09 +0000

    Design for menu icon button

--- lib/screens/components/widget/display_interests.dart
+++ lib/screens/components/widget/display_interests.dart
@@ -9,7 +9,6 @@ import 'package:project_seg/services/user_state.dart';
 import 'package:provider/provider.dart';
 import 'package:project_seg/constants/colours.dart';
 
-
 class DisplayInterests extends StatelessWidget {
   const DisplayInterests({
     Key? key,
@@ -57,7 +56,7 @@ class DisplayInterests extends StatelessWidget {
       label: label,
       capitalizeLabel: true,
       mini: mini,
-      textColor: kSimpleWhiteColour,
+      textColor: kWhiteColour,
       onTap: getOnTap(label, context),
     );
   }
@@ -74,7 +73,8 @@ class DisplayInterests extends StatelessWidget {
           context: context,
           builder: (BuildContext context) {
             return EditDialogChipDisplay(
-              values: _userState.user?.userData?.categorizedInterests ?? CategorizedInterests(categories: []),
+              values: _userState.user?.userData?.categorizedInterests ??
+                  CategorizedInterests(categories: []),
               onSave: _onSave,
             );
           },
--- lib/screens/home/feed/feed_screen.dart
+++ lib/screens/home/feed/feed_screen.dart
@@ -22,10 +22,12 @@ import 'package:project_seg/constants/colours.dart';
 class FeedScreen extends StatefulWidget {
   FeedScreen({Key? key}) : super(key: key);
 
-  static PageController controller = PageController(viewportFraction: 1, keepPage: true);
+  static PageController controller =
+      PageController(viewportFraction: 1, keepPage: true);
 
   static void animateToTop() {
-    controller.animateToPage(0, duration: const Duration(milliseconds: 500), curve: Curves.ease);
+    controller.animateToPage(0,
+        duration: const Duration(milliseconds: 500), curve: Curves.ease);
   }
 
   @override
@@ -81,15 +83,23 @@ class _FeedScreenState extends State<FeedScreen> {
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.only(top: 50, right: leftRightPadding),
-                    child: FloatingActionButton(
-                      heroTag: null,
-                      onPressed: () => context.pushNamed(editPreferencesScreenName, params: {pageParameterKey: feedScreenName}),
-                      backgroundColor: kTertiaryColour,
-                      child: Icon(
-                        Icons.menu,
-                        color: kWhiteColour,
-                        size: 30,
+                    padding:
+                        const EdgeInsets.only(top: 48, right: leftRightPadding),
+                    child: Container(
+                      decoration: const BoxDecoration(boxShadow: [
+                        BoxShadow(color: kSecondaryColour, blurRadius: 60.0),
+                      ]),
+                      child: IconButton(
+                        // heroTag: null,
+                        onPressed: () => context.pushNamed(
+                            editPreferencesScreenName,
+                            params: {pageParameterKey: feedScreenName}),
+                        // backgroundColor: kTertiaryColour,
+                        icon: const Icon(
+                          Icons.menu,
+                          color: kWhiteColour,
+                          size: 30,
+                        ),
                       ),
                     ),
                   ),

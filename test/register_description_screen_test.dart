f414d65ce3ab3139f54f3196c6812d7571f3de58
Luca Brown <luca.brown@kcl.ac.uk>
2022-03-29 14:20:16 +0100

    Refactor the WidgetPumper.

--- test/register_description_screen_test.dart
+++ test/register_description_screen_test.dart
@@ -5,24 +5,23 @@ import 'package:flutter/material.dart';
 import 'package:project_seg/models/User/user_data.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:project_seg/screens/components/buttons/university_button.dart';
+import 'package:project_seg/screens/sign_up/register_description_screen.dart';
 import 'mock.dart';
 import 'test_resources/WidgetPumper.dart';
 
-
 void main() {
   setupFirebaseAuthMocks();
-  UserData currentUserData = UserData(
-    firstName: "Amy"
-  );
+  UserData currentUserData = UserData(firstName: "Amy");
 
   setUpAll(() async {
     await Firebase.initializeApp();
     //final TestWidgetsFlutterBinding binding = TestWidgetsFlutterBinding.ensureInitialized();
   });
 
-  testWidgets('description page has all info widgets', (WidgetTester tester) async{
-    ValueKey key = const ValueKey("description page test");
-    await WidgetPumper.pumpRegisterDescriptionScreen(tester, key, currentUserData);
+  testWidgets('Description page has all info widgets',
+      (WidgetTester tester) async {
+    await WidgetPumper.pumpCustomWidget(
+        tester, RegisterDescriptionScreen(userData: currentUserData));
 
     final Finder iconButton = find.byType(IconButton);
     expect(iconButton, findsOneWidget);
@@ -30,11 +29,12 @@ void main() {
     final Finder textFinder = find.text('... and a bit more about Amy');
     expect(textFinder, findsOneWidget);
 
-    final Finder univeristyFinder = find.text('UNIVERSITY');
-    expect(univeristyFinder, findsOneWidget);
+    final Finder universityFinder = find.text('UNIVERSITY');
+    expect(universityFinder, findsOneWidget);
 
-    final Finder univeristyWidgetTextFinder = find.text('Select your university');
-    expect(univeristyWidgetTextFinder, findsOneWidget);
+    final Finder universityFinderTextFinder =
+        find.text('Select your university');
+    expect(universityFinderTextFinder, findsOneWidget);
 
     final Finder universityWidgetFinder = find.byType(UniversityButton);
     expect(universityWidgetFinder, findsOneWidget);
@@ -48,25 +48,24 @@ void main() {
     final Finder selectUniveristyWidgetTextFinder = find.text("King's College London");
     expect(selectUniveristyWidgetTextFinder, findsOneWidget); */
 
-
     final Finder bioTextFinder = find.text('BIO / SHORT DESCRIPTION');
     expect(bioTextFinder, findsOneWidget);
 
-    final Finder bioWidgetFinder = find.widgetWithText(TextFormField, 'Enter your bio here...');
+    final Finder bioWidgetFinder =
+        find.widgetWithText(TextFormField, 'Enter your bio here...');
     expect(bioWidgetFinder, findsOneWidget);
 
     await tester.enterText(bioWidgetFinder, "This is my bio.");
     await tester.pumpAndSettle();
 
-    final Finder textBioWidgetFinder = find.widgetWithText(TextFormField, "This is my bio.");
+    final Finder textBioWidgetFinder =
+        find.widgetWithText(TextFormField, "This is my bio.");
     expect(textBioWidgetFinder, findsOneWidget);
 
-    final Finder nextTextFinder = find.text ('Next');
+    final Finder nextTextFinder = find.text('Next');
     expect(nextTextFinder, findsOneWidget);
 
     final Finder nextButtonFinder = find.widgetWithText(ElevatedButton, 'Next');
     expect(nextButtonFinder, findsOneWidget);
-
   });
 }
-

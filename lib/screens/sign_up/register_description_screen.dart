4f5e178d2f91917356eebe4b452eedecac21bf28
Adamski, Mateusz (k20063061) <mateusz.adamski@kcl.ac.uk>
2022-03-30 15:23:35 +0100

    Add comments to screens/components/sign_up.

--- lib/screens/sign_up/register_description_screen.dart
+++ lib/screens/sign_up/register_description_screen.dart
@@ -13,13 +13,21 @@ import 'package:provider/provider.dart';
 
 import '../../constants/borders.dart';
 
+/**
+ * The fourth screen that is displayed through the sign up process.
+ * The user is asked to provide details about the university they attend
+ * and specify their bio.
+ */
+
 class RegisterDescriptionScreen extends StatefulWidget {
-  const RegisterDescriptionScreen({Key? key, required this.userData}) : super(key: key);
+  const RegisterDescriptionScreen({Key? key, required this.userData})
+      : super(key: key);
 
   final UserData userData;
 
   @override
-  _RegisterDescriptionScreenState createState() => _RegisterDescriptionScreenState();
+  _RegisterDescriptionScreenState createState() =>
+      _RegisterDescriptionScreenState();
 }
 
 class _RegisterDescriptionScreenState extends State<RegisterDescriptionScreen> {
@@ -70,7 +78,8 @@ class _RegisterDescriptionScreenState extends State<RegisterDescriptionScreen> {
               expandedHeight: 150,
               collapsedHeight: 130,
               leading: IconButton(
-                onPressed: () => context.goNamed(registerPhotoScreenName, extra: widget.userData),
+                onPressed: () => context.goNamed(registerPhotoScreenName,
+                    extra: widget.userData),
                 icon: const Icon(Icons.arrow_back_ios, color: primaryColour),
                 //buildIcons(Icons.arrow_back_ios, kPrimaryColour),
               ),
@@ -79,10 +88,14 @@ class _RegisterDescriptionScreenState extends State<RegisterDescriptionScreen> {
                 height: double.infinity,
                 alignment: Alignment.bottomCenter,
                 child: Padding(
-                  padding: const EdgeInsets.fromLTRB(leftRightPadding, 5, leftRightPadding, 5),
+                  padding: const EdgeInsets.fromLTRB(
+                      leftRightPadding, 5, leftRightPadding, 5),
                   child: Text(
                     '... and a bit more about ${widget.userData.firstName}',
-                    style: Theme.of(context).textTheme.headline4?.apply(color: secondaryColour, fontWeightDelta: 2),
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline4
+                        ?.apply(color: secondaryColour, fontWeightDelta: 2),
                   ),
                 ),
               ),
@@ -100,7 +113,10 @@ class _RegisterDescriptionScreenState extends State<RegisterDescriptionScreen> {
                         children: <Widget>[
                           Text(
                             'UNIVERSITY',
-                            style: Theme.of(context).textTheme.bodyLarge?.apply(fontWeightDelta: 1),
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyLarge
+                                ?.apply(fontWeightDelta: 1),
                           ),
                         ],
                       ),
@@ -109,7 +125,8 @@ class _RegisterDescriptionScreenState extends State<RegisterDescriptionScreen> {
                         editable: true,
                         shouldExpand: true,
                         color: secondaryColour,
-                        label: widget.userData.university ?? "Select your university",
+                        label: widget.userData.university ??
+                            "Select your university",
                         onSave: (university) => setState(() {
                           widget.userData.university = university;
                         }),
@@ -120,7 +137,10 @@ class _RegisterDescriptionScreenState extends State<RegisterDescriptionScreen> {
                         children: [
                           Text(
                             'BIO / SHORT DESCRIPTION',
-                            style: Theme.of(context).textTheme.bodyLarge?.apply(fontWeightDelta: 1),
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyLarge
+                                ?.apply(fontWeightDelta: 1),
                           ),
                         ],
                       ),
@@ -132,13 +152,16 @@ class _RegisterDescriptionScreenState extends State<RegisterDescriptionScreen> {
                           maxLines: 10,
                           textAlignVertical: TextAlignVertical.top,
                           decoration: const InputDecoration(
-                            border: OutlineInputBorder(borderRadius: BorderRadius.all(radius10), borderSide: BorderSide.none),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(radius10),
+                                borderSide: BorderSide.none),
                             labelText: "Enter your bio here...",
                             floatingLabelBehavior: FloatingLabelBehavior.never,
                             filled: true,
                             fillColor: lightTertiaryColour,
                           ),
-                          onChanged: (value) => widget.userData.bio = value.trim(),
+                          onChanged: (value) =>
+                              widget.userData.bio = value.trim(),
                           keyboardType: TextInputType.text,
                           textCapitalization: TextCapitalization.sentences),
                       ValidationError(errorText: validateBioError),
@@ -147,11 +170,14 @@ class _RegisterDescriptionScreenState extends State<RegisterDescriptionScreen> {
                         width: double.infinity,
                         child: PillButtonFilled(
                           text: "Next",
-                          textStyle: const TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+                          textStyle: const TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.w600),
                           onPressed: () {
-                            if (!_key.currentState!.validate() || !validate()) return;
+                            if (!_key.currentState!.validate() || !validate())
+                              return;
 
-                            context.goNamed(registerInterestsScreenName, extra: widget.userData);
+                            context.goNamed(registerInterestsScreenName,
+                                extra: widget.userData);
                           },
                         ),
                       ),

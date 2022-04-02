0f22c3826516878543e285849ef6a741923112c6
Luca Brown <luca.brown@kcl.ac.uk>
2022-04-02 11:23:38 +0100

    Complete services documentation

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -3,6 +3,11 @@ import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart' as mockAuth;
 
+/// The entry point for authentication services.
+///
+/// This class uses [FirebaseAuth] to provide authentication services
+/// to the whole application.Through [firebaseAuth] you can access
+/// the logged in user's data as well as listen for any user changes.
 class AuthService {
   final auth.FirebaseAuth firebaseAuth;
 
@@ -10,28 +15,37 @@ class AuthService {
 
   AuthService({required this.firebaseAuth});
 
+  /// Gets changes to any user updates.
   Stream<auth.User?> get user {
     return firebaseAuth.userChanges();
   }
 
+  /// Returns the currently logged in user, if it exists.
   auth.User? get currentUser {
     return firebaseAuth.currentUser;
   }
 
+  /// Signs the user in with the given [email] and [password].
   Future<void> signIn(String email, String password) async {
-    await firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+    await firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
   }
 
+  /// Signs the user up with the given [email] and [password].
   Future<void> signUp(String email, String password) async {
-    await firebaseAuth.createUserWithEmailAndPassword(email: email, password: password).then((_) async {
+    await firebaseAuth
+        .createUserWithEmailAndPassword(email: email, password: password)
+        .then((_) async {
       await sendVerificationEmail();
     });
   }
 
+  /// Signs the [currentUser] out.
   Future<void> signOut() async {
     return await firebaseAuth.signOut();
   }
 
+  /// Deletes the [currentUser] upon [password] validation.
   Future<void> deleteAccount(String password) async {
     final verified = await validatePassword(password);
     if (verified) {
@@ -39,6 +53,7 @@ class AuthService {
     }
   }
 
+  /// Sends a verification email to the [currentUser].
   Future<void> sendVerificationEmail() async {
     // For testing purposes - mock package does not support emails.
     if (firebaseAuth is mockAuth.MockFirebaseAuth) return;
@@ -46,6 +61,9 @@ class AuthService {
     await currentUser?.sendEmailVerification(null);
   }
 
+  /// Refreshes the [currentUser], if signed in.
+  /// 
+  /// Catches an [auth.FirebaseAuthException] if the user is not signed in.
   Future<void> reloadUser() async {
     try {
       await firebaseAuth.currentUser?.reload();
@@ -55,19 +73,28 @@ class AuthService {
     }
   }
 
+  /// Periodically checks if the [currentUser]'s email has been verified.
+  /// 
+  /// This method starts a periodic [timer] that will automatically 
+  /// stop after the [currentUser] has been verified.
   void startCheckingForVerifiedEmail() {
-    timer = Timer.periodic(const Duration(seconds: 1), (timer) async {
-      await reloadUser();
-      if (currentUser?.emailVerified == true) {
-        timer.cancel();
-      }
-    });
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      (timer) async {
+        await reloadUser();
+        if (currentUser?.emailVerified == true) {
+          timer.cancel();
+        }
+      },
+    );
   }
 
+  /// Cancels the [timer] if it has been set.
   void stopCheckingForVerifiedEmail() {
     timer?.cancel();
   }
 
+  /// Sends a password reset email to the [currentUser].
   Future<void> resetPassword(String email) async {
     // For testing purposes - mock package does not support emails.
     if (firebaseAuth is mockAuth.MockFirebaseAuth) return;
@@ -75,7 +102,9 @@ class AuthService {
     return await firebaseAuth.sendPasswordResetEmail(email: email);
   }
 
-  Future<void> changePassword(String currentPassword, String newPassword) async {
+/// Changes the [currentUser]'s [currentPassword] to the given [newPassword].
+  Future<void> changePassword(
+      String currentPassword, String newPassword) async {
     auth.User? user = currentUser;
     if (user == null) {
       return;
@@ -85,13 +114,15 @@ class AuthService {
     await user.updatePassword(newPassword);
   }
 
+  /// Authenticates the [currentUser] using fresh credentials.
   Future<bool> validatePassword(String password) async {
     auth.User? user = currentUser;
     if (user == null) {
       return false;
     }
 
-    final credentials = auth.EmailAuthProvider.credential(email: user.email!, password: password);
+    final credentials = auth.EmailAuthProvider.credential(
+        email: user.email!, password: password);
     await user.reauthenticateWithCredential(credentials);
     return true;
   }
--- lib/services/feed_content_controller.dart
+++ lib/services/feed_content_controller.dart
@@ -8,6 +8,13 @@ import 'package:project_seg/services/firestore_service.dart';
 import 'package:project_seg/states/recommendations_state.dart';
 import 'package:project_seg/states/user_state.dart';
 
+/// This class is responsible for managing, updating and displaying
+/// the queue of recommended users.
+///
+/// The [FeedContentController] gets its [content] from the [FeedContentGatherer],
+/// that is otherwise initialized with a [FeedLoadingSheet]. The [FeedContentGatherer]
+/// then updates the [content] when the queue is finally loaded and is responsible
+/// for removing, inserting and refreshing the [content].
 class FeedContentController extends ChangeNotifier {
   final _desiredFeedContentLength = 5;
   final UserState userState;
@@ -21,18 +28,26 @@ class FeedContentController extends ChangeNotifier {
 
   FeedContentController({required this.userState});
 
+  /// Initializes the [PageController].
+  ///
+  /// This method is responsible for adding a listener to the page [controller]
+  /// and then initializing the [gatherer] to start listening for new [content].
   void assignController(PageController controller) {
     controller.addListener(() => pageChangeListener(controller));
     gatherer = FeedContentGatherer(
         userState: userState,
         onLikeComplete: (likedUser) {
-          controller.nextPage(duration: const Duration(milliseconds: 500), curve: Curves.ease);
+          controller.nextPage(
+              duration: const Duration(milliseconds: 500), curve: Curves.ease);
           gatherer?.removeLiked();
         });
   }
 
   bool removalTriggered = false;
 
+  /// Notifies all [FeedContentController] listeners of page changes, if any.
+  ///
+  /// The method also removes the second to last profile that has been scrolled off the screen.
   void pageChangeListener(PageController controller) async {
     double? page = controller.page;
 
@@ -51,36 +66,45 @@ class FeedContentController extends ChangeNotifier {
     }
   }
 
+  /// Removes the [ProfileContainer] at the front of the queue.
   void removeAtFront(int amount) {
     for (int i = 0; i < amount; i++) {
       content.removeAt(0);
     }
   }
 
+  /// Removes all the [ProfileContainer]s from the queue.
   void removeAll() {
     content.removeWhere((element) => element.runtimeType != FeedLoadingSheet);
   }
 
+
+  /// Inserts a [ProfileContainer] at the end of the queue.
+  /// 
+  /// [content] is only updated if its size goes below the [_desiredFeedContentLength].
   Future<void> insertAtEnd() async {
     if (_desiredFeedContentLength - 2 > (content.length - 1)) {
-      //print("Requesting new content");
       final entries = await gatherer?.gather(_desiredFeedContentLength);
       if (entries != null) {
         content.addAll(entries);
       }
       moveLoadingScreenLast();
     }
-    //print("Content: ${(content.length - 1)}");
   }
 
+  /// Moves the [FeedLoadingSheet] to the end of the queue.
   void moveLoadingScreenLast() {
-    final index = content.indexWhere((Widget element) => element.runtimeType == FeedLoadingSheet);
+    final index = content.indexWhere(
+        (Widget element) => element.runtimeType == FeedLoadingSheet);
     final loadingScreen = content.removeAt(index);
     content.add(loadingScreen);
   }
 
+  /// Clears the [gatherer]'s queue and listens for new recommendations.
   void onFeedInitialized(FirestoreService firestoreService) {
-    final RecommendationsState _recState = RecommendationsState(userState.user!.user!, firestoreService: firestoreService);
+    final RecommendationsState _recState = RecommendationsState(
+        userState.user!.user!,
+        firestoreService: firestoreService);
 
     _recState.addListener(() async {
       removeAll();
@@ -92,6 +116,7 @@ class FeedContentController extends ChangeNotifier {
     });
   }
 
+  /// Refreshes the [content].
   Future<void> refreshContent() async {
     removeAll();
     notifyListeners();
@@ -100,6 +125,7 @@ class FeedContentController extends ChangeNotifier {
   }
 }
 
+/// A widget that displays a loading screen while the feed is being loaded.
 class FeedLoadingSheet extends StatelessWidget {
   const FeedLoadingSheet({Key? key}) : super(key: key);
 
@@ -114,7 +140,8 @@ class FeedLoadingSheet extends StatelessWidget {
               Align(
                 alignment: Alignment.centerRight,
                 child: Padding(
-                  padding: const EdgeInsets.only(top: leftRightPadding, right: leftRightPadding),
+                  padding: const EdgeInsets.only(
+                      top: leftRightPadding, right: leftRightPadding),
                   child: Transform.scale(
                     scale: 4,
                     child: SizedBox(
@@ -131,7 +158,8 @@ class FeedLoadingSheet extends StatelessWidget {
                       aspectRatio: 1.2,
                       child: SizedBox(
                         width: double.infinity,
-                        child: Lottie.asset('assets/lotties/searching.json', fit: BoxFit.cover),
+                        child: Lottie.asset('assets/lotties/searching.json',
+                            fit: BoxFit.cover),
                       ),
                     ),
                     Text(
@@ -139,9 +167,11 @@ class FeedLoadingSheet extends StatelessWidget {
                       style: Theme.of(context).textTheme.headline4,
                     ),
                     const SizedBox(height: 20),
-                    const Text("Give us a minute while we search for your next match."),
+                    const Text(
+                        "Give us a minute while we search for your next match."),
                     const SizedBox(height: 10),
-                    const Text("If this is taking too long, try editing your preferences.")
+                    const Text(
+                        "If this is taking too long, try editing your preferences.")
                   ],
                 ),
               ),
